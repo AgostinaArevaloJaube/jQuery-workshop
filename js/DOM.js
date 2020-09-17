2655db4df83b67ef9141f8fda1eea319ba99fe00
Jesus <jesusignaciolencina@gmail.com>
2020-09-17 20:49:34 -0300

    Actualización DOM-js

---
+++ js/DOM.js
@@ -0,0 +1,8 @@
+//DOM
+let textoFrase = document.getElementById("textoFrase");
+let autorFrase = document.getElementById("autorFrase");
+//PRUEBA
+textoFrase.innerHTML = arrayQuotes[1].quote;
+autorFrase.innerHTML = arrayQuotes[1].author;
+
+//dfasdasdasd
