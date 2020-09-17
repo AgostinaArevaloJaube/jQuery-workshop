ef5ed6c65c3b8dd39cc6610fef4c28364ce4fdac
damiloyola <damiloyola@gmail.com>
2020-09-17 20:57:32 -0300

    se pone funcion e nmail

--- js/main.js
+++ js/main.js
@@ -1,17 +1,14 @@
-//ARRAY DE QUOTES
-var arrayQuotes = [];
+let urlFull = "http://quotes.stormconsultancy.co.uk/quotes.json";
 
-//DOM
-let textoFrase = document.getElementById("textoFrase");
-let autorFrase = document.getElementById("autorFrase");
+fetch(urlFull)
+  .then((data) => data.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
 
-//PRUEBA
-textoFrase.innerHTML = "HOLA";
-autorFrase.innerHTML = "sdfsdfsdf";
-
-//
-textoFrase.innerHTML = "HOLA";
-
-autorFrase.innerHTML = "HOLA";
-
-//
+let randomQuote = async () => {
+  let arr = await fetch(urlFull)
+    .then((data) => data.json())
+    .then((data) => data);
+  let randomNum = Math.floor(Math.random() * arr.length);
+  llenarHTML(arr[randomNum]);
+};
