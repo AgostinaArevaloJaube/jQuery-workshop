cf73b5561ebe079a5abbe69d8148293ce3e76efd
damiloyola <damiloyola@gmail.com>
2020-09-17 20:58:04 -0300

    borro fetch

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 let urlFull = "http://quotes.stormconsultancy.co.uk/quotes.json";
 
-fetch(urlFull)
-  .then((data) => data.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
-
 let randomQuote = async () => {
   let arr = await fetch(urlFull)
     .then((data) => data.json())
