495097cc1eb6a648a2c046006338cedeed4b9a1e
Jesus <jesusignaciolencina@gmail.com>
2020-09-21 19:02:53 -0300

    Fetcheo de API
    Una excelente adaptación

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
-let urlFull = "http://quotes.stormconsultancy.co.uk/quotes.json";
+//DOM
+let tituloNoticia = document.getElementById("articleTitle");
+let contenidoNoticia = document.getElementById("articleP");
+let fotoNoticia = document.getElementById("articleIMG");
+
+
+const apiKey = "djGmUVY2p7uVirNFUoIUDryw2UKKpLci";
+let urlFull = "https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=" + apiKey;
 let randomQuote = async () => {
   let arr = await fetch(urlFull)
     .then((data) => data.json())
     .then((data) => data);
   let randomNum = Math.floor(Math.random() * arr.length);
   console.log(arr);
-  llenarHTML(arr[randomNum]);
+  llenarHTML(arr);
 };
 
 function llenarHTML(objeto) {
-  textoFrase.innerHTML = objeto.quote;
-  console.log(objeto.quote);
-  autorFrase.innerHTML = objeto.quote;
-  console.log(objeto.quote);
+  tituloNoticia.innerHTML = objeto.results[0].title;
+  console.log(objeto.title);
+  contenidoNoticia.innerHTML = objeto.results[0].abstract;
+  console.log(objeto.abstract);
+  fotoNoticia.src = objeto.results[0].multimedia[2].url;
 }
 
 randomQuote();
